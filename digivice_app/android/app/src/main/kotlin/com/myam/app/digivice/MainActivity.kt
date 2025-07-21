cb5c13f1372ed6cb953f1f7882b3094dc405df37
Marco Yam <marco.yam.catina@gmail.com>
2025-07-21 17:25:58 -0600

    Add Windows runner support with DPI awareness and console output
    - Implemented a manifest file for the Windows runner to enable DPI awareness.
    - Created utility functions for console attachment and command line argument parsing.
    - Developed the Win32Window class to manage window creation, message handling, and DPI scaling.
    - Added header files for utility functions and the Win32 window class.

---
+++ digivice_app/android/app/src/main/kotlin/com/myam/app/digivice/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.myam.app.digivice
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ digivice_app/lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:digivice_app/app/app.dart';
+import 'package:digivice_app/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
